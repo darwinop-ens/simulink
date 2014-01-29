a1befefdf68a3d521e6cd5df9a067400760cbe34
Florent Ouchet <florent.ouchet@ens-rennes.fr>
2014-01-29 22:22:46 +0100

    darwinop_consts to return a structure and minimize number of left-hand arguments.

--- darwinoplib/darwinoplib_setup.m
+++ darwinoplib/darwinoplib_setup.m
@@ -25,8 +25,8 @@ for f=1:length(listing)
     if strncmp(filename, ref, length(ref)) && (filename(end-1) == '.') && ...
        ((filename(end) == 'M') || (filename(end) == 'm'))
         language_files{i} = filename(1:end-2);
-        [~,~,~,~,~,~,language] = eval(filename(1:end-2));
-        languages{i} = language;
+        consts = eval(filename(1:end-2));
+        languages{i} = consts.language;
         i = i+1;
     end
 end
